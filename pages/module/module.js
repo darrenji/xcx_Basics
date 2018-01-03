6356f050c11ef0325e8e61b26f8302885146a42b
darren <764190362@qq.com>
2018-01-03 17:29:25 +0800

    text组件

--- pages/module/module.js
+++ pages/module/module.js
@@ -1,11 +1,22 @@
+var initData = 'this is first line \n this is second line';
+var extraLine = [];
+
 Page({
-  data: {
-    iconSize: [20, 30, 40, 50, 60, 70],
-    iconColor: [
-      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple'
-    ],
-    iconType: [
-      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
-    ]
+  data:{
+    text: initData
+  },
+  add: function(e){
+    extraLine.push('other line');
+    this.setData({
+      text: initData + '\n' + extraLine.join('\n')
+    });
+  },
+  remove: function(){
+    if(extraLine.length>0){
+      extraLine.pop();
+      this.setData({
+        text: initData + '\n' + extraLine.join('\n')
+      });
+    }
   }
 })
