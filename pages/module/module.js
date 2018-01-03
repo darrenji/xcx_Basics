05d6ed5c00c9bf613df4a0655b3828a1e61f7ac4
darren <764190362@qq.com>
2018-01-03 16:31:39 +0800

    swiper组件，change事件

--- pages/module/module.js
+++ pages/module/module.js
@@ -6,5 +6,8 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ]
+  },
+  swiperChange: function(e){
+    console.log(e.detail);
   }
 })
