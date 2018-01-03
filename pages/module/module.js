9115266eacbfa6a45bcd68762d364aa9de06c380
darren <764190362@qq.com>
2018-01-03 22:06:12 +0800

    checkbox组件，一般的显示

--- pages/module/module.js
+++ pages/module/module.js
@@ -1,48 +1,69 @@
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    checkboxItems: [
-      { name: 'USA', value: '美国' },
-      { name: 'CHN', value: '中国', checked: 'true' },
-      { name: 'BRA', value: '巴西' },
-      { name: 'JPN', value: '日本', checked: 'true' },
-      { name: 'ENG', value: '英国' },
-      { name: 'TUR', value: '法国' },
-    ],
-    radioItems: [
-      { name: 'USA', value: '美国' },
-      { name: 'CHN', value: '中国', checked: 'true' },
-      { name: 'BRA', value: '巴西' },
-      { name: 'JPN', value: '日本' },
-      { name: 'ENG', value: '英国' },
-      { name: 'TUR', value: '法国' },
-    ],
-    hidden: false
+    items: [
+      {name: 'USA', value: '美国'},
+      {name: 'CHN', value: '中国', checked: 'true'},
+      {name: 'BRA', value: '巴西'}
+    ]
   },
-  checkboxChange: function (e) {
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
     
-    //["CHN", "JPN", "BRA"] 也就是把选中项的name属性放到数组里
-    var checked = e.detail.value
-    var changed = {}
-    for (var i = 0; i < this.data.checkboxItems.length; i++) {
-      //如果当前遍历的元素不在当前数组中
-      if (checked.indexOf(this.data.checkboxItems[i].name) !== -1) {
-        changed['checkboxItems[' + i + '].checked'] = true
-      } else {
-        changed['checkboxItems[' + i + '].checked'] = false
-      }
-    }
-    this.setData(changed)
   },
-  radioChange: function (e) {
-    var checked = e.detail.value
-    var changed = {}
-    for (var i = 0; i < this.data.radioItems.length; i++) {
-      if (checked.indexOf(this.data.radioItems[i].name) !== -1) {
-        changed['radioItems[' + i + '].checked'] = true
-      } else {
-        changed['radioItems[' + i + '].checked'] = false
-      }
-    }
-    this.setData(changed)
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
