f533ae70efac975851d32a5c4d38c7480b91a971
darren <764190362@qq.com>
2018-01-03 08:02:06 +0800

    冒泡事件，点击内层事件触发外层事件

--- pages/test/test.js
+++ pages/test/test.js
@@ -3,43 +3,13 @@ var common = require('../../common/common.js');
 
 Page({
   data:{
-    name: 'darren',
-    pass: null,
-    id: 1,
-    condition: false,
-    arr: [1, 2, 3, 4, 5],
-    obj1: {
-      a:1,
-      b:2
-    }
+
   },
-  toHome: function(){
-    wx.switchTab({
-      url: '../index/index',
-    })
+  innerTap: function(){
+    console.log('触发了innerTap事件');
   },
-  toNavi: function(){
-    wx.navigateTo({
-      url: '../navi/navi',
-      success: function(){
-        console.log('跳转到navi成功')
-      }
-    })
-  },
-  say: function(){
-    common.sayHello(this.data.name);
-  },
-  switch: function(){
-    this.setData({
-      condition: !this.data.condition
-    });
-  },
-  onLoad: function(){
-    this.setData({
-      pass: app.globalData.pass
-    });
-  },
-  onShow: function(){
-    console.log("test页的onShow");
+  middleTap: function(){
+    console.log('触发了outerTap事件');
   }
+
 })
