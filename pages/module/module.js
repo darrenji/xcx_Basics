c6c1eb38d42914403bdb0d781cb87fdab8ec4d62
darren <764190362@qq.com>
2018-01-03 15:55:05 +0800

    动态设置scroll-into-view属性

--- pages/module/module.js
+++ pages/module/module.js
@@ -1,5 +1,9 @@
+var order = ["a","b","c"];
+var index = 0;
 Page({
-
+  data:{
+    v: "a"
+  },
   scrolltoupper: function(e){
     console.log(e);
     console.log('触发了toUpper');
@@ -9,5 +13,14 @@ Page({
   },
   scroll: function(e){
     console.log(e);
+  },
+  tapChange: function(){
+    index++;
+    if(index > order.length-1){
+      index = 0;
+    }
+    this.setData({
+      v:order[index]
+    });
   }
 })
