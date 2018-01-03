b5e90f91c46a3927003afc93f169f44a880b2baa
darren <764190362@qq.com>
2018-01-03 17:15:14 +0800

    icon组件

--- pages/module/module.js
+++ pages/module/module.js
@@ -1,12 +1,11 @@
-
 Page({
-  onReady() {
-    this.videoCtx = wx.createVideoContext('myVideo')
-  },
-  play() {
-    this.videoCtx.play()
-  },
-  pause() {
-    this.videoCtx.pause()
+  data: {
+    iconSize: [20, 30, 40, 50, 60, 70],
+    iconColor: [
+      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple'
+    ],
+    iconType: [
+      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
+    ]
   }
 })
