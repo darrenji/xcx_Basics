22267a1fc4c05cdedc5926099733a954f93842cb
darren <764190362@qq.com>
2018-01-03 16:48:07 +0800

    swiper组件，动态改变interval属性值

--- pages/module/module.js
+++ pages/module/module.js
@@ -5,9 +5,16 @@ Page({
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ]
+    ],
+    inter: 500
   },
   swiperChange: function(e){
-    console.log(e.detail);
+    console.log(e.detail);  
+  },
+  intervalChange: function(e){
+    var sliderValue = e.detail.value;
+    this.setData({
+      inter: sliderValue
+    });
   }
 })
