c4875a98bbd9b274d169134c87cb3e207b2107e8
darren <764190362@qq.com>
2018-01-03 21:02:54 +0800

    checkbox组件

--- pages/module/module.js
+++ pages/module/module.js
@@ -1,40 +1,15 @@
-var types = ['default', 'primary', 'warn']
-var pageObject = {
+Page({
   data: {
-    defaultSize: 'default',
-    primarySize: 'default',
-    warnSize: 'default',
-    disabled: false,
-    plain: false,
-    loading: false
+    items: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国', checked: 'true' },
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本' },
+      { name: 'ENG', value: '英国' },
+      { name: 'TUR', value: '法国' },
+    ]
   },
-  setDisabled: function (e) {
-    this.setData({
-      disabled: !this.data.disabled
-    })
-  },
-  setPlain: function (e) {
-    this.setData({
-      plain: !this.data.plain
-    })
-  },
-  setLoading: function (e) {
-    this.setData({
-      loading: !this.data.loading
-    })
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
   }
-}
-
-for (var i = 0; i < types.length; ++i) {
-  (function (type) {
-    pageObject[type] = function (e) {
-      var key = type + 'Size'
-      var changedData = {}
-      changedData[key] =
-        this.data[key] === 'default' ? 'mini' : 'default'
-      this.setData(changedData)
-    }
-  })(types[i])
-}
-
-Page(pageObject)
+})
